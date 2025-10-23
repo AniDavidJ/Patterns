b68a92d59bc238479e0b9d8b65ac20132102a33c
AniDavidJ <anidavid07@gmail.com>
2025-10-23 23:46:31 +0400

    feat: Add math.js file with rounding and truncation examples

---
+++ math.js
@@ -0,0 +1,13 @@
+// const value = 1e2;
+// console.log(value);
+// const math = Number.MAX_SAFE_INTEGER
+// console.log(math+2);
+
+const roundValue = Math.round(1.4);
+console.log(roundValue);
+const ceilValue = Math.ceil(1.2);
+console.log(ceilValue);
+const floorValue = Math.floor(1.9);
+console.log(floorValue);
+const truncValue = Math.trunc(1.99);
+console.log(truncValue);
